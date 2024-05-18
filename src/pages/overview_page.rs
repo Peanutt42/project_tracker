d8544a0f5c7cec6c02c95d178eb691b041736de9
Peanutt42 <p3anutt42@gmail.com>
2024-05-18 23:15:40 +0200

    Made loading screen only replace widgets that aren't loaded yet

--- src/pages/overview_page.rs
+++ src/pages/overview_page.rs
@@ -1,5 +1,5 @@
 use iced::{widget::{column, row, container, text}, Element, Length};
-use crate::project_tracker::{ProjectTrackerApp, UiMessage};
+use crate::{components::loading_screen, project_tracker::{ProjectTrackerApp, UiMessage}};
 
 #[derive(Debug, Clone)]
 pub struct OverviewPage {
@@ -13,23 +13,28 @@ impl OverviewPage {
 		}
 	}
 
-	pub fn view<'a>(&'a self, _app: &'a ProjectTrackerApp) -> Element<UiMessage> {
-		column![
-			text("Todo Overview").size(35),
-			row![
-				container(
-					text("Todo").size(20)
-				)
-				.width(Length::FillPortion(1)),
-				
-				container(
-					text("In Progress").size(20)
-				)
-				.width(Length::FillPortion(1)),
+	pub fn view<'a>(&'a self, app: &'a ProjectTrackerApp) -> Element<UiMessage> {
+		if let Some(_saved_state) = &app.saved_state {
+			column![
+				text("Todo Overview").size(35),
+				row![
+					container(
+						text("Todo").size(20)
+					)
+					.width(Length::FillPortion(1)),
+					
+					container(
+						text("In Progress").size(20)
+					)
+					.width(Length::FillPortion(1)),
+				]
+				.width(Length::Fill)
 			]
-			.width(Length::Fill)
-		]
-		.spacing(10)
-		.into()
+			.spacing(10)
+			.into()
+		}
+		else {
+			loading_screen()
+		}
 	}
 }
--- src/pages/sidebar_page.rs
+++ src/pages/sidebar_page.rs
@@ -1,6 +1,7 @@
 use iced::{widget::{scrollable, container, Column, column}, Element, Length, alignment::Horizontal};
 use crate::components::{create_new_project_button, loading_screen, overview_button, project_preview, horizontal_seperator};
 use crate::project_tracker::{ProjectTrackerApp, UiMessage};
+use crate::project::Project;
 
 #[derive(Debug, Clone)]
 pub struct SidebarPage {
@@ -14,39 +15,53 @@ impl SidebarPage {
 		}
 	}
 
+	fn project_preview_list<'a>(projects: &'a [Project], app: &'a ProjectTrackerApp, overview_button: Column<'a, UiMessage>) -> Element<'a, UiMessage> {
+		let mut list: Vec<Element<UiMessage>> = projects.iter()
+		.map(|project| {
+			let selected = project.name == app.selected_page_name;
+			project_preview(project, selected)
+		})
+		.collect();
+		list.insert(0, overview_button.into());
+
+		scrollable(
+			Column::from_vec(list)
+				.width(Length::Fill)
+				.spacing(10)
+		)
+		.width(Length::Fill)
+		.into()
+	}
+
 	pub fn view<'a>(&'a self, app: &'a ProjectTrackerApp) -> Element<UiMessage> {
-		if let Some(saved_state) = &app.saved_state {
-			let mut selectables: Vec<Element<UiMessage>> = saved_state.projects.iter()
-				.map(|project| {
-					let selected = project.name == app.selected_page_name;
-					project_preview(project, selected)
-				})
-				.collect();
-				
-			selectables.insert(0, column![
-				overview_button(app.content_page.is_overview_page()),
-				horizontal_seperator(5.0),
-			]
-			.spacing(10)
-			.into());
+		let overview_button = column![
+			overview_button(app.content_page.is_overview_page()),
+			horizontal_seperator(5.0),
+		]
+		.spacing(10);
 
-			column![
-				container(create_new_project_button())
-					.align_x(Horizontal::Right)
-					.width(Length::Fill),
+		let list: Element<UiMessage> =
+			if let Some(saved_state) = &app.saved_state {
+				Self::project_preview_list(&saved_state.projects, app, overview_button)
+			}
+			else {
+				column![
+					overview_button,
+					loading_screen(),
+				]
+				.width(Length::Fill)
+				.spacing(10)
+				.into()
+			};
 
-				scrollable(
-					Column::from_vec(selectables)
-						.width(Length::Fill)
-						.spacing(10)
-				)
+		column![
+			container(create_new_project_button())
+				.align_x(Horizontal::Right)
 				.width(Length::Fill),
-			]
-			.spacing(5)
-			.into()
-		}
-		else {
-			loading_screen()
-		}
+
+			list,
+		]
+		.spacing(5)
+		.into()
 	}
 }
