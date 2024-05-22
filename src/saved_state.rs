039c55badd5bcc515d3afe99af1fdc3003ffe245
Peanutt42 <p3anutt42@gmail.com>
2024-05-22 16:09:18 +0200

    Projects are now saved into program preferences instead of working dir

--- src/saved_state.rs
+++ src/saved_state.rs
@@ -1,4 +1,5 @@
 use serde::{Serialize, Deserialize};
+use std::path::PathBuf;
 use crate::project::Project;
 use crate::theme_mode::ThemeMode;
 
@@ -9,24 +10,41 @@ pub struct SavedState {
 }
 
 impl SavedState {
+	async fn filepath() -> PathBuf {
+		let project_dirs = directories::ProjectDirs::from("", "", "ProjectTracker")
+		.expect("Failed to get saved state filepath");
+
+		let data_dir = project_dirs.data_local_dir();
+
+		tokio::fs::create_dir_all(data_dir).await.expect("Failed to create Local Data Directories");
+
+		data_dir
+			.join("project_tracker_db.json")
+			.to_path_buf()
+	}
+
 	pub async fn load() -> SavedState {
-		match tokio::fs::read_to_string("save.project_tracker").await {
+		let filepath = Self::filepath().await;
+
+		match tokio::fs::read_to_string(filepath.clone()).await {
 			Ok(file_content) => {
 				serde_json::from_str(&file_content).unwrap_or_else(|_| {
-					println!("Failed to load previous projects");
+					println!("Failed to load previous projects in {}", filepath.display());
 					SavedState::default()
 				})
 			},
 			Err(_) => {
-				println!("Could not find previous projects");
+				println!("Could not find previous projects in {}", filepath.display());
 				SavedState::default()
 			}
 		}
 	}
 
 	pub async fn save(self) {
-		if let Err(e) = tokio::fs::write("save.project_tracker", serde_json::to_string_pretty(&self).unwrap().as_bytes()).await {
-			eprintln!("Failed to save: {e}");
+		let filepath = Self::filepath().await;
+
+		if let Err(e) = tokio::fs::write(filepath.clone(), serde_json::to_string_pretty(&self).unwrap().as_bytes()).await {
+			eprintln!("Failed to save to {}: {e}", filepath.display());
 		}
 	}
 }
