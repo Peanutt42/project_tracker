6b1c000973be72816037919f336a270a2cfeb516
Peanutt42 <p3anutt42@gmail.com>
2024-06-22 21:56:56 +0200

    Made proper icon + linux window is now associated with the app

--- build.rs
+++ build.rs
@@ -2,10 +2,10 @@ use std::{env, io};
 use winresource::WindowsResource;
 
 fn main() -> io::Result<()> {
-	// this sets the icon of the bin to icon-handdraw.ico for windows
+	// this sets the icon of the bin to icon.ico for windows
     if env::var_os("CARGO_CFG_WINDOWS").is_some() {
         WindowsResource::new()
-            .set_icon("assets/icon-handdrawn.ico")
+            .set_icon("assets/icon.ico")
             .compile()?;
     }
     Ok(())
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 // only enables the 'windows' subsystem when compiling in release
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use iced::{Settings, Application, Font, window::{self, icon}};
+use iced::{Settings, Application, Font, window::{self, icon, settings::PlatformSpecific}};
 use project_tracker_gui::ProjectTrackerApp;
 
 fn main() -> Result<(), iced::Error> {
 	ProjectTrackerApp::run(Settings {
 		window: window::Settings {
 			icon: icon::from_file_data(
-				include_bytes!("../assets/icon-handdrawn.png"),
+				include_bytes!("../assets/icon.png"),
 				Some(image::ImageFormat::Png)
 			).ok(),
 			exit_on_close_request: false,
+			#[cfg(target_os = "linux")]
+			platform_specific: PlatformSpecific { application_id: "Project Tracker".to_string() },
 			..Default::default()
 		},
 		antialiasing: true,
