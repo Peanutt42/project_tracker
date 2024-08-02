683474580fb1524fcb7dd32d123cbdfd95fa3c73
Peanutt42 <p3anutt42@gmail.com>
2024-08-02 05:36:39 -0400

    added tooltips to task tags

--- project_tracker_gui/src/core/task_tag.rs
+++ project_tracker_gui/src/core/task_tag.rs
@@ -1,7 +1,7 @@
 use iced_aw::{quad::Quad, widgets::InnerBounds};
 use serde::{Deserialize, Serialize};
-use iced::{Border, Color, Element, Length};
-use crate::{core::SerializableColor, project_tracker::UiMessage};
+use iced::{theme, widget::{text, tooltip, tooltip::Position}, Border, Color, Element, Length};
+use crate::{core::SerializableColor, project_tracker::UiMessage, styles::{RoundedContainerStyle, SMALL_TEXT_SIZE}};
 
 #[derive(Debug, Clone, Copy, Serialize, Deserialize, PartialEq, Eq, Hash)]
 pub struct TaskTagId(pub usize);
@@ -28,14 +28,20 @@ impl TaskTag {
 
 	pub fn view(&self) -> Element<UiMessage> {
 		let color: Color = self.color.into();
-		Quad {
-			width: Length::Fixed(50.0),
-			height: Length::Fixed(5.0),
-			inner_bounds: InnerBounds::Ratio(1.0, 1.0),
-			quad_color: color.into(),
-			quad_border: Border::with_radius(f32::MAX),
-			..Default::default()
-		}
+		tooltip(
+			Quad {
+				width: Length::Fixed(50.0),
+				height: Length::Fixed(5.0),
+				inner_bounds: InnerBounds::Ratio(1.0, 1.0),
+				quad_color: color.into(),
+				quad_border: Border::with_radius(f32::MAX),
+				..Default::default()
+			},
+			text(&self.name).size(SMALL_TEXT_SIZE),
+			Position::Top
+		)
+		.gap(5)
+		.style(theme::Container::Custom(Box::new(RoundedContainerStyle)))
 		.into()
 	}
 }
