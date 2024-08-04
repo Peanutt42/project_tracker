27b94f76c1a57e18f940175e7522c9fa36366ea8
Peanutt42 <p3anutt42@gmail.com>
2024-08-04 13:21:09 -0400

    filepath now has rounded background

--- project_tracker_gui/src/components/file_location.rs
+++ project_tracker_gui/src/components/file_location.rs
@@ -1,19 +1,22 @@
 use std::path::Path;
 use iced::{theme, widget::{container, row, scrollable, text}, Alignment, Element, Padding};
-use crate::{project_tracker::UiMessage, styles::{scrollable_horizontal_direction, ScrollableStyle, SCROLLBAR_WIDTH, SMALL_PADDING_AMOUNT, SPACING_AMOUNT}};
+use crate::{project_tracker::UiMessage, styles::{scrollable_horizontal_direction, RoundedContainerStyle, ScrollableStyle, SCROLLBAR_WIDTH, SMALL_HORIZONTAL_PADDING, SMALL_PADDING_AMOUNT, SPACING_AMOUNT}};
 use crate::components::open_location_button;
 
 pub fn filepath_widget(filepath: &Path) -> Element<'static, UiMessage> {
 	scrollable(
 		container(
 			container(
-				text(
-					filepath.display()
-				)
+				text(filepath.display())
 			)
-			.style(theme::Container::Box)
+			.style(theme::Container::Custom(Box::new(RoundedContainerStyle)))
+			.padding(SMALL_HORIZONTAL_PADDING)
 		)
-		.padding(Padding{ bottom: SCROLLBAR_WIDTH + SMALL_PADDING_AMOUNT, top: SCROLLBAR_WIDTH + SMALL_PADDING_AMOUNT, ..Padding::ZERO })
+		.padding(Padding{
+			top: SCROLLBAR_WIDTH + SMALL_PADDING_AMOUNT,
+			bottom: SCROLLBAR_WIDTH + SMALL_PADDING_AMOUNT,
+			..Padding::ZERO
+		})
 	)
 	.direction(scrollable_horizontal_direction())
 	.style(theme::Scrollable::custom(ScrollableStyle))
