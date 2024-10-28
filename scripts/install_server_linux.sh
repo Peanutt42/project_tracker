548ca6ba73937bb927af6f6732c1273ea6793224
Peanutt42 <p3anutt42@gmail.com>
2024-10-28 12:30:23 +0100

    made install_server_linux.sh stop and start the service

--- scripts/install_server_linux.sh
+++ scripts/install_server_linux.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 
-project_root="$(realpath "../")"
+project_root="$(realpath $(dirname $0)/../)"
 
 if [ "$project_root" = "" ]; then
 	echo "failed to get absolute filepath of project root"
 	exit 1
 fi
 
-cd "$project_root/project_tracker_server"
+echo "Stopping service if already running"
+sudo systemctl stop ProjectTrackerServer.service >/dev/null 2>&1
 
 echo "Compiling..."
-
+cd "$project_root/project_tracker_server"
 cargo b --release
 
-cd "$project_root/scripts"
-
-database_file="/srv/project_tracker_server/database.json"
-
 echo "Enabling systemd service..."
-
+cd "$project_root/scripts"
+database_file="/srv/project_tracker_server/database.json"
 service_file="$project_root/target/release/ProjectTrackerServer.service"
-
 echo "[Unit]" > "$service_file"
 echo "Description=Runs the server to host the Project Tracker synchronization" >> "$service_file"
 echo "" >> "$service_file"
@@ -29,7 +26,9 @@ echo "ExecStart=$project_root/target/release/project_tracker_server $database_fi
 echo "" >> "$service_file"
 echo "[Install]" >> "$service_file"
 echo "WantedBy=default.target" >> "$service_file"
-
 sudo systemctl enable "$service_file"
+
+echo "Starting service..."
+sudo systemctl start ProjectTrackerServer.service
 
 echo "Finished!"
