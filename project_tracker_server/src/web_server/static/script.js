846c68c3351102230db3d5c4cd0a59b66c849389
Peanutt42 <p3anutt42@gmail.com>
2024-11-27 00:09:00 +0100

    improved web server ui with dark theme

--- project_tracker_server/src/web_server/static/script.js
+++ project_tracker_server/src/web_server/static/script.js
@@ -1,13 +1,33 @@
-document.getElementById("submit").addEventListener("click", submitPassword);
+const database_json_output = document.getElementById("database-json-output");
+const login_page = document.getElementById("login_page");
+const password_input = document.getElementById("password_input");
+const login_button = document.getElementById("login_button");
+const show_password = document.getElementById("show_password");
+const show_password_checkbox = document.getElementById("show_password_checkbox");
+const invalid_password = document.getElementById("invalid_password");
 
-document.getElementById("password").addEventListener("keypress", (event) => {
+login_button.addEventListener("click", submitPassword);
+
+password_input.addEventListener("keypress", (event) => {
     if (event.key === "Enter") {
         submitPassword();
     }
 });
 
+show_password.addEventListener("click", toggleShowPassword);
+show_password_checkbox.addEventListener("click", toggleShowPassword);
+show_password_checkbox.checked = false;
+
+function toggleShowPassword() {
+	if (show_password_checkbox.checked) {
+		password_input.type = "text";
+	} else {
+		password_input.type = "password";
+	}
+}
+
 async function submitPassword() {
-		const password = document.getElementById("password").value;
+	const password = password_input.value;
 
     if (!password) {
         alert("Please enter a password!");
@@ -24,12 +44,19 @@ async function submitPassword() {
         if (response.ok) {
           const json = await response.json();
           const prettyJson = JSON.stringify(json, null, 2);
-          document.getElementById("database-json-output").textContent = prettyJson;
+          database_json_output.textContent = prettyJson;
+		  login_page.style.display = "none";
+		  password_input.classList.remove("invalid");
+		  invalid_password.style.display = "none";
         } else if (response.status === 401) {
       		// TODO: show a error msg above password input and make input red etc.
-					document.getElementById("database-json-output").textContent = "Invalid password!";
+			login_page.style.display = "block";
+			password_input.classList.add("invalid");
+			invalid_password.style.display = "block";
         } else {
-					document.getElementById("database-json-output").textContent = "An error occurred: " + response.statusText;
+			login_page.style.display = "block";
+			password_input.classList.add("invalid");
+			invalid_password.style.display = "block";
           alert("An error occurred: " + response.statusText);
         }
     } catch (error) {
