c65085f8d175b6eb0e21b46e0fcc9843045282c2
Peanutt42 <p3anutt42@gmail.com>
2024-12-01 00:58:26 +0100

    web-server: simple database json processing to show task names

--- project_tracker_server/src/web_server/static/script.js
+++ project_tracker_server/src/web_server/static/script.js
@@ -1,4 +1,4 @@
-const database_json_output = document.getElementById("database-json-output");
+const database_list = document.getElementById("database");
 const logout_button = document.getElementById("logout_button");
 const login_page = document.getElementById("login_page");
 const password_input = document.getElementById("password_input");
@@ -25,14 +25,14 @@ show_password.addEventListener("click", toggleShowPassword);
 show_password_checkbox.addEventListener("click", toggleShowPassword);
 show_password_checkbox.checked = false;
 
-// auto login if last login was successful
+// auto logidatabase_listn if last login was successful
 document.addEventListener("DOMContentLoaded", () => {
 	const stored_password = localStorage.getItem("password");
 	if (stored_password) {
 		hide_login_page();
 		const last_loaded_database = localStorage.getItem("last_loaded_database");
 		if (last_loaded_database) {
-			database_json_output.textContent = last_loaded_database;
+			populate_dom_from_database(last_loaded_database);
 		}
 		login(stored_password);
 	}
@@ -92,13 +92,12 @@ async function login(password) {
 		});
 
 		if (response.ok) {
-			const json = await response.json();
-			const prettyJson = JSON.stringify(json, null, 2);
-			database_json_output.textContent = prettyJson;
+			const database = await response.json();
+			populate_dom_from_database(database);
 			hide_login_page();
 			style_valid_password();
 			localStorage.setItem("password", password);
-			localStorage.setItem("last_loaded_database", prettyJson);
+			localStorage.setItem("last_loaded_database", database);
 		} else if (response.status === 401) {
 			logout();
 			style_invalid_password();
@@ -115,9 +114,56 @@ async function login(password) {
 }
 
 function logout() {
-	database_json_output.textContent = "";
+	database_list.innerHTML = "";
 	show_login_page();
 	localStorage.removeItem("password");
+}
+
+function populate_dom_from_database(database) {
+	database_list.innerHTML = "";
+	for (const project_id in database.projects) {
+		const project = database.projects[project_id];
+		const project_div = document.createElement("div");
+		project_div.className = "project";
+		project_div.textContent = project.name + ':';
+		const task_list = document.createElement("ul");
+		task_list.className = "task_list";
+		for (const task_id in project.todo_tasks) {
+			populate_dom_with_task(task_list, project.todo_tasks[task_id], 'todo');
+		}
+		for (const task_id in project.source_code_todos) {
+			populate_dom_with_task(task_list, project.source_code_todos[task_id], 'source code todo');
+		}
+		for (const task_with_id of project.done_tasks) {
+			populate_dom_with_task(task_list, task_with_id[1], 'done');
+		}
+		project_div.appendChild(task_list);
+		database_list.appendChild(project_div);
+	}
+}
+
+function populate_dom_with_task(task_list, task, task_type) {
+	const task_div = document.createElement("div");
+	task_div.className = "task";
+	let task_info = task_type + ' - ';
+	if (task.time_spend_seconds) {
+		task_info += Math.floor(task.time_spend_seconds / 60) + 'min';
+		if (task.needed_time_minutes === null) {
+			task_info += '/... - ';
+		}
+	}
+	if (task.needed_time_minutes) {
+		if (task.time_spend_seconds) {
+			task_info += '/';
+		}
+		task_info += task.needed_time_minutes + 'min - ';
+	}
+	if (task.due_date) {
+		task_info += task.due_date.day + '.' + task.due_date.month + '.' + task.due_date.year + ' - ';
+	}
+	task_info += task.name;
+	task_div.textContent = task_info;
+	task_list.appendChild(task_div);
 }
 
 function connect_ws() {
