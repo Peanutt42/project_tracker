59a068e37839f18d5b46b32e1d81dcac7a4fd174
Peanutt42 <p3anutt42@gmail.com>
2025-01-05 21:51:18 +0100

    fixed slow encryption in debug (/"dev") mode
    1. switched to argon2 for password hashing
    2. enabled optimisation of encryption libraries in debug mode

--- project_tracker_server/src/encryption.rs
+++ project_tracker_server/src/encryption.rs
@@ -1,31 +1,55 @@
 use aes_gcm::aead::Aead;
-use aes_gcm::{Aes256Gcm, Error, Key, KeyInit, Nonce};
-use pbkdf2::pbkdf2_hmac;
+use aes_gcm::{Aes256Gcm, Key, KeyInit, Nonce};
+use argon2::{password_hash::SaltString, Algorithm, Argon2, Params, Version};
 use rand::Rng;
-use sha2::Sha256;
+use thiserror::Error;
 
-const PBKDF2_ITERATIONS: u32 = 100_000; // Increase for more security in production
 const KEY_LENGTH: usize = 32; // 32 bytes for AES-256
 pub const NONCE_LENGTH: usize = 12; // AES-GCM standard nonce length
 pub const SALT_LENGTH: usize = 16; // Salt length
 
-fn derive_key(password: &str, salt: &[u8; SALT_LENGTH]) -> [u8; KEY_LENGTH] {
-	let mut key = [0u8; KEY_LENGTH];
-	pbkdf2_hmac::<Sha256>(password.as_bytes(), salt, PBKDF2_ITERATIONS, &mut key);
-	key
+#[derive(Error, Debug, Clone)]
+pub enum EncryptionError {
+	#[error("failed to encrypt/decrypt")]
+	AesGcm(#[from] aes_gcm::Error),
+	#[error("failed to hash password with argon2")]
+	Argon2(#[from] argon2::Error),
+}
+
+fn derive_key(password: &str, salt: &[u8; SALT_LENGTH]) -> argon2::Result<[u8; KEY_LENGTH]> {
+	let mut output_key = [0u8; KEY_LENGTH];
+
+	// if this default value changes in the future --> rethink the 2 specified in the 'Params'
+	static_assertions::const_assert_eq!(Params::DEFAULT_P_COST, 1);
+
+	let argon2 = Argon2::new(
+		Algorithm::Argon2id,
+		Version::V0x13,
+		Params::new(Params::DEFAULT_M_COST, Params::DEFAULT_T_COST, 2, None)?,
+	);
+
+	let salt = SaltString::encode_b64(salt).unwrap();
+
+	argon2.hash_password_into(
+		password.as_bytes(),
+		salt.as_str().as_bytes(),
+		&mut output_key,
+	)?;
+
+	Ok(output_key)
 }
 
 #[allow(clippy::type_complexity)]
 pub fn encrypt(
 	content: &[u8],
 	password: &str,
-) -> Result<(Vec<u8>, [u8; SALT_LENGTH], [u8; NONCE_LENGTH]), Error> {
+) -> Result<(Vec<u8>, [u8; SALT_LENGTH], [u8; NONCE_LENGTH]), EncryptionError> {
 	let mut salt = [0u8; SALT_LENGTH];
 	rand::thread_rng().fill(&mut salt);
 	let mut nonce = [0u8; NONCE_LENGTH];
 	rand::thread_rng().fill(&mut nonce);
 
-	let key = derive_key(password, &salt);
+	let key = derive_key(password, &salt)?;
 	let cipher = Aes256Gcm::new(Key::<Aes256Gcm>::from_slice(&key));
 
 	let ciphertext = cipher.encrypt(Nonce::from_slice(&nonce), content)?;
@@ -37,10 +61,10 @@ pub fn decrypt(
 	password: &str,
 	salt: &[u8; SALT_LENGTH],
 	nonce: &[u8; NONCE_LENGTH],
-) -> Result<Vec<u8>, Error> {
-	let key = derive_key(password, salt);
+) -> Result<Vec<u8>, EncryptionError> {
+	let key = derive_key(password, salt)?;
 	let cipher = Aes256Gcm::new(Key::<Aes256Gcm>::from_slice(&key));
 	let nonce = Nonce::from_slice(nonce);
 
-	cipher.decrypt(nonce, encrypted)
+	cipher.decrypt(nonce, encrypted).map_err(|e| e.into())
 }
