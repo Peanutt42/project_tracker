33e99d257a2ca618e314541cfe0e4b6bfd105094
Peanutt42 <p3anutt42@gmail.com>
2025-01-23 00:08:28 +0100

    made checkbox check mark white

--- src/styles/checkbox_style.rs
+++ src/styles/checkbox_style.rs
@@ -3,22 +3,23 @@ use iced::{
 	widget::checkbox::{Status, Style},
 	Border, Theme,
 };
+use iced_aw::style::colors::WHITE;
 
 pub fn checkbox_style(theme: &Theme, status: Status) -> Style {
 	match status {
 		Status::Active { is_checked } => Style {
 			background: if is_checked {
-				theme.extended_palette().primary.base.color.into()
+				theme.extended_palette().primary.strong.color.into()
 			} else {
 				theme.extended_palette().background.base.color.into()
 			},
-			icon_color: theme.extended_palette().success.base.text,
+			icon_color: WHITE,
 			text_color: None,
 			border: Border {
-				radius: 3.0.into(),
+				radius: 2.5.into(),
 				width: 1.0,
 				color: if is_checked {
-					theme.extended_palette().primary.base.color
+					theme.extended_palette().primary.strong.color
 				} else {
 					theme.extended_palette().background.weak.color
 				},
@@ -27,21 +28,21 @@ pub fn checkbox_style(theme: &Theme, status: Status) -> Style {
 		Status::Hovered { is_checked } | Status::Disabled { is_checked } => Style {
 			background: if is_checked {
 				color_average(
-					theme.extended_palette().primary.base.color,
+					theme.extended_palette().primary.strong.color,
 					theme.extended_palette().background.base.text,
 				)
 				.into()
 			} else {
 				theme.extended_palette().background.weak.color.into()
 			},
-			icon_color: theme.extended_palette().success.base.text,
+			icon_color: WHITE,
 			text_color: None,
 			border: Border {
-				radius: 3.0.into(),
+				radius: 2.5.into(),
 				width: 1.0,
 				color: if is_checked {
 					color_average(
-						theme.extended_palette().primary.base.color,
+						theme.extended_palette().primary.strong.color,
 						theme.extended_palette().background.base.text,
 					)
 				} else {
